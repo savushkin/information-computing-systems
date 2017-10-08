7857fc507328e55a43dc5e851989386815f5a169
makhnovets <lerunchicm@gmail.com>
2017-10-08 23:17:29 +0300

    add module for lab4 in my fork try 2

---
+++ lab4/src/main/java/me/savushkin/lab4/Lab.java
@@ -0,0 +1,13 @@
+package me.savushkin.lab4;
+
+
+import java.util.logging.Logger;
+
+public class Lab {
+
+
+    static Logger log = Logger.getLogger(Lab.class.getName());
+
+    public static void main(String[] args) {
+    }
+}
