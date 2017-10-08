163c60585cea3c1e9fbdd6a49a885847806cbe49
makhnovets <lerunchicm@gmail.com>
2017-10-08 23:03:42 +0300

    add module for lab6 try 2

---
+++ lab6/src/main/java/me/savushkin/lab6/Lab.java
@@ -0,0 +1,26 @@
+package me.savushkin.lab6;
+
+import me.savushkin.common.LabHelper;
+import oracle.jdbc.pool.OracleConnectionPoolDataSource;
+import oracle.jdbc.pool.OracleDataSource;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import javax.sql.PooledConnection;
+import java.io.File;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Hashtable;
+import java.util.logging.Logger;
+
+public class Lab {
+
+
+    public static void main(String[] args) {
+
+    }
+}
