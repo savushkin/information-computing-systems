f1f4ef2f5e811ac9d685258c3e50e7e4b5420edd
lerunchicm@gmail.com <aiY9johxna>
2017-10-17 01:20:56 +0300

    add properties for lab4

--- lab4/src/main/java/me/savushkin/lab4/Lab.java
+++ lab4/src/main/java/me/savushkin/lab4/Lab.java
@@ -7,13 +7,11 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.sql.*;
 import java.util.Arrays;
 import java.util.Hashtable;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 public class Lab {
@@ -22,5 +20,61 @@ public class Lab {
 
     public static void main(String[] args) {
 
+    }
+
+    //get property
+    private Properties getProperties() {
+        Properties prop = new Properties();
+        InputStream input = null;
+        try {
+
+            input = new FileInputStream("database.properties");
+
+            // load a properties file
+            prop.load(input);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return prop;
+    }
+
+    //set property
+    private static void setProperty() {
+
+        Properties prop = new Properties();
+        OutputStream output = null;
+        try {
+
+            output = new FileOutputStream("database.properties");
+
+            // set the properties value
+            prop.setProperty("jdbc.drivers", "oracle.jdbc.OracleDriver");
+            prop.setProperty("jdbcUrl", "jdbcoraclethin@localhost1521orbis");
+            prop.setProperty("dbuser", "mkyong");
+            prop.setProperty("dbpassword", "password");
+
+            // save properties to project root folder
+            prop.store(output, null);
+
+        } catch (IOException io) {
+            io.printStackTrace();
+        } finally {
+            if (output != null) {
+                try {
+                    output.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
