0bbd6ceb7bf6d52f8d557a01c816fdef1143e333
Ivan Savushkin <ivan@savushkin.me>
2017-10-23 00:27:06 +0300

    lab8

--- lab8/src/main/java/me/savushkin/lab8/Lab.java
+++ lab8/src/main/java/me/savushkin/lab8/Lab.java
@@ -1,217 +1,81 @@
 package me.savushkin.lab8;
 
 import me.savushkin.common.LabHelper;
-import oracle.jdbc.pool.OracleDataSource;
 import oracle.jdbc.rowset.OracleCachedRowSet;
 import oracle.jdbc.rowset.OracleJDBCRowSet;
-import oracle.sql.ROWID;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
-import java.io.*;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.RowId;
-import java.sql.SQLException;
-import java.util.Hashtable;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.sql.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class Lab {
+    // 182190
+    // var. 23
+
+    // 1. Подготовить предложение SELECT для выполнения запроса в соответствии с выданным преподавателем вариантом.
+    // 2. Выполнить запрос и получить результат в объект типа ResultSet.
+    // 3. Создать объект типа CachedRowSet и заполнить его результатами запроса, находящимися в объекте типа ResultSet, методом populate().
+    // 4. Сохранить объект типа CachedRowSet в файле, имя которого должно совпадать с идентификатором студента, выполняющего задание, и находиться в корне его домашнего каталога.
+
+    // 23. Определите вашу среднюю оценку (естественно, что в расчет должны входить лишь те оценки, которые имеют цифровой эквивалент). Создайте два запроса, в которых средняя оценки определяется с помощью функции среднее значение (функция AVG) и путем деления суммы (функция SUM) оценок на их количество (функция COUNT). В результате необходимо оставить два десятичных знака после запятой.
+    // Для округления используется функция ROUND(expr [,m]), возвращающая expr, округленное до m-го десятичного знака; если m опущено, то оно принимается равным 0, а если m < 0, то округляются цифры левее десятичной точки.
     private final static String ORACLE_URL = "jdbc:oracle:thin:@localhost:1521:orbis";
     private final static String ORACLE_DRIVER = "oracle.jdbc.driver.OracleDriver";
-    private final static String ORACLE_USER = "";
-    private final static String ORACLE_PASS = "";
-    private final static String POSTGRES_URL = "jdbc:postgresql://pg:5432/ucheb";
-    private final static String POSTGRES_DRIVER = "org.postgresql.Driver";
-    private final static String POSTGRES_USER = "";
-    private final static String POSTGRES_PASS = "";
+    private final static String ORACLE_USER = "s182190";
+    private final static String ORACLE_PASS = "tny395";
 
-    private final static String DATA_SOURCE_NAME = "myDatabase";
+    private final static List<String> QUERY_MARK = Arrays.asList(
+            "SELECT Л.ФАМИЛИЯ, Л.ИМЯ, Л.ОТЧЕСТВО, О.СРЕДНЯЯ_ОЦЕНКА FROM Н_ЛЮДИ Л JOIN (SELECT В.ЧЛВК_ИД, ROUND(AVG(CAST(В.ОЦЕНКА AS DECIMAL(1, 0))), 2) СРЕДНЯЯ_ОЦЕНКА FROM Н_ВЕДОМОСТИ В WHERE В.ОЦЕНКА IN ('2','3','4','5') GROUP BY В.ЧЛВК_ИД) О ON Л.ИД = О.ЧЛВК_ИД WHERE Л.ИД = ?",
+            "SELECT Л.ФАМИЛИЯ, Л.ИМЯ, Л.ОТЧЕСТВО, О.СРЕДНЯЯ_ОЦЕНКА FROM Н_ЛЮДИ Л JOIN (SELECT В.ЧЛВК_ИД, ROUND(SUM(CAST(В.ОЦЕНКА AS DECIMAL(1, 0))) / COUNT(В.ОЦЕНКА), 2) СРЕДНЯЯ_ОЦЕНКА FROM Н_ВЕДОМОСТИ В WHERE В.ОЦЕНКА IN ('2','3','4','5') GROUP BY В.ЧЛВК_ИД) О ON Л.ИД = О.ЧЛВК_ИД WHERE Л.ИД = ?"
+    );
 
-    private final static String CRS_FILE_LOC_TASK3 ="cachedrs_task3.crs";
-    private final static String CRS_FILE_LOC_TASK4 ="cachedrs_task4.crs";
+    private final static String STUDENT_ID = "114092";
 
-    private static Context context = null;
 
-    public static void main(String[] args) {
-        if (args.length > 0) {
-            switch (args[0]) {
-                case "task1": {
-                    try {
-                        OracleJDBCRowSet oracleJDBCRowSet = new OracleJDBCRowSet();
-                        oracleJDBCRowSet.setUrl(ORACLE_URL);
-                        oracleJDBCRowSet.setUsername(ORACLE_USER);
-                        oracleJDBCRowSet.setPassword(ORACLE_PASS);
-                        oracleJDBCRowSet.setCommand("SELECT count(*) FROM н_люди");
-                        oracleJDBCRowSet.execute();
-                        LabHelper.printResultSet(oracleJDBCRowSet);
-                    } catch (SQLException se) {
-                        se.printStackTrace();
+    public static void main(String[] argv) {
+        try {
+            Class.forName(ORACLE_DRIVER);
+
+            try (Connection connection = DriverManager.getConnection(ORACLE_URL, ORACLE_USER, ORACLE_PASS)) {
+                for (String query: QUERY_MARK)
+                    try (OracleCachedRowSet oracleCachedRowSet = new OracleCachedRowSet();) {
+                        oracleCachedRowSet.setCommand(query);
+                        try (ObjectOutputStream objectOutputStream =
+                                     new ObjectOutputStream(
+                                             new FileOutputStream(oracleCachedRowSet.getTableName()))) {
+                            objectOutputStream.writeObject(oracleCachedRowSet);
+                            objectOutputStream.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+
+//                    while(resultSet.next()) {
+//                        int columnsCount = metaData.getColumnCount();
+//                        for (int column = 1; column <= columnsCount; column++) {
+//                            StringBuilder stringBuilder = new StringBuilder();
+//                            stringBuilder.append(metaData.getColumnName(column)).append(": ");
+//                            switch (metaData.getColumnType(column)) {
+//                                case 2:
+//                                    stringBuilder.append(resultSet.getDouble(column));
+//                                    break;
+//                                case 12:
+//                                    stringBuilder.append(resultSet.getString(column));
+//                                    break;
+//                            }
+//                            System.out.println(stringBuilder.toString());
+//                        }
+//                        System.out.println();
+//                    }
+
                     }
-                    break;
-                }
-                case "task2": {
-                    try {
-                        OracleJDBCRowSet oracleJDBCRowSet = new OracleJDBCRowSet();
-                        oracleJDBCRowSet.setUrl(ORACLE_URL);
-                        oracleJDBCRowSet.setUsername(ORACLE_USER);
-                        oracleJDBCRowSet.setPassword(ORACLE_PASS);
-                        oracleJDBCRowSet.setCommand("SELECT * FROM н_люди WHERE ид = ?");
-                        oracleJDBCRowSet.setInt(1,161148);
-                        oracleJDBCRowSet.execute();
-                        LabHelper.printResultSet(oracleJDBCRowSet);
-                    } catch (SQLException se) {
-                        se.printStackTrace();
-                    }
-                    break;
-                }
-                case "task3": {
-                    File jnfiDir = new File("JNDI");
-                    if (!jnfiDir.exists())
-                        jnfiDir.mkdir();
-
-                    try {
-                        Hashtable environment = new Hashtable();
-                        environment.put (Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.fscontext.RefFSContextFactory");
-                        environment.put (Context.PROVIDER_URL, "file:JNDI");
-                        context = new InitialContext(environment);
-
-                        bindDataSource(context, DATA_SOURCE_NAME);
-
-                        //Create serialized CachedRowSet
-                        writeCachedRowSet();
-
-                        //Create CachedRowSet from serialized object
-                        OracleCachedRowSet oracleCachedRowSet = readCachedRowSet();
-
-                        //Display values
-                        LabHelper.printResultSet(oracleCachedRowSet);
-                        //Close resource
-                        oracleCachedRowSet.close();
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
-                    break;
-                }
-                case "task4": {
-                    try {
-                        System.out.println("load data...");
-                        OracleCachedRowSet oracleCachedRowSet = new OracleCachedRowSet();
-                        oracleCachedRowSet.setUrl(ORACLE_URL);
-                        oracleCachedRowSet.setUsername(ORACLE_USER);
-                        oracleCachedRowSet.setPassword(ORACLE_PASS);
-                        oracleCachedRowSet.setCommand("SELECT * FROM номера_телефонов");
-                        oracleCachedRowSet.execute();
-                        LabHelper.printResultSet(oracleCachedRowSet);
-                        FileOutputStream fileOutputStream = new FileOutputStream(CRS_FILE_LOC_TASK4);
-                        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-                        objectOutputStream.writeObject(oracleCachedRowSet);
-                        objectOutputStream.close();
-
-                        System.out.println("read data...");
-                        FileInputStream fileInputStream = new FileInputStream(CRS_FILE_LOC_TASK4);
-                        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-                        oracleCachedRowSet = (OracleCachedRowSet)objectInputStream.readObject();
-                        fileInputStream.close();
-                        objectInputStream.close();
-                        oracleCachedRowSet.setCommand("SELECT * FROM номера_телефонов");
-                        oracleCachedRowSet.execute();
-                        LabHelper.printResultSet(oracleCachedRowSet);
-
-                        System.out.println("update row...");
-                        oracleCachedRowSet.setReadOnly(false);
-                        oracleCachedRowSet.first();
-                        oracleCachedRowSet.updateString(2,"1131313");
-                        oracleCachedRowSet.updateRow();
-                        oracleCachedRowSet.acceptChanges();
-                        LabHelper.printResultSet(oracleCachedRowSet);
-
-                        System.out.println("update row cancel...");
-                        oracleCachedRowSet.setReadOnly(false);
-                        oracleCachedRowSet.last();
-                        oracleCachedRowSet.updateString(2,"42424242");
-                        oracleCachedRowSet.updateRow();
-                        oracleCachedRowSet.cancelRowUpdates();
-                        LabHelper.printResultSet(oracleCachedRowSet);
-
-                        System.out.println("insert row...");
-                        oracleCachedRowSet.moveToInsertRow();
-                        oracleCachedRowSet.updateInt(1,120848);
-                        oracleCachedRowSet.updateString(2,"9332323");
-                        oracleCachedRowSet.updateDate(3, new Date(System.currentTimeMillis()));
-                        oracleCachedRowSet.updateInt(4,1);
-                        oracleCachedRowSet.insertRow();
-                        oracleCachedRowSet.acceptChanges();
-                        LabHelper.printResultSet(oracleCachedRowSet);
-
-                        System.out.println("insert row cancel...");
-                        oracleCachedRowSet.moveToInsertRow();
-                        oracleCachedRowSet.updateInt(1,120848);
-                        oracleCachedRowSet.updateString(2,"9332323");
-                        oracleCachedRowSet.updateDate(3, new Date(System.currentTimeMillis()));
-                        oracleCachedRowSet.updateInt(4,1);
-                        oracleCachedRowSet.insertRow();
-                        oracleCachedRowSet.restoreOriginal();
-                        LabHelper.printResultSet(oracleCachedRowSet);
-                        oracleCachedRowSet.close();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-
-                    break;
-                }
-                default:
-                    System.err.println("Undefined argument");
-                    break;
             }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
         }
-        System.out.println("Goodbye!");
-    }
-
-    public static void writeCachedRowSet() throws Exception {
-        Connection connection;
-        DataSource dataSource;
-        dataSource = (DataSource) context.lookup(DATA_SOURCE_NAME);
-        connection = dataSource.getConnection();
-
-        //Instantiate a CachedRowSet object, set connection parameters
-        OracleCachedRowSet oracleCachedRowSet = new OracleCachedRowSet();
-
-        //Set and execute the command. Notice the parameter query.
-        oracleCachedRowSet.setCommand("SELECT * FROM н_люди WHERE ид = ?");
-        oracleCachedRowSet.setInt(1,161148);
-        oracleCachedRowSet.execute(connection);
-
-        //Serialize CachedRowSet object.
-        FileOutputStream fileOutputStream = new FileOutputStream(CRS_FILE_LOC_TASK3);
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-        objectOutputStream.writeObject(oracleCachedRowSet);
-        objectOutputStream.close();
-        oracleCachedRowSet.close();
-        connection.close();
-    }//end writeCachedRowSet()
-
-    public static OracleCachedRowSet readCachedRowSet() throws Exception{
-        //Read serialized CachedRowSet object from storage
-        FileInputStream fileInputStream = new FileInputStream(CRS_FILE_LOC_TASK3);
-        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-        OracleCachedRowSet oracleCachedRowSet = (OracleCachedRowSet)objectInputStream.readObject();
-        fileInputStream.close();
-        objectInputStream.close();
-        return oracleCachedRowSet;
-    }//end readCachedRowSet()
-
-    public static void bindDataSource(Context context, String dataSourceName)
-            throws SQLException, NamingException {
-        OracleDataSource oracleDataSource = new OracleDataSource();
-        oracleDataSource.setUser(ORACLE_USER);
-        oracleDataSource.setPassword(ORACLE_PASS);
-        oracleDataSource.setDriverType("thin");
-        oracleDataSource.setDatabaseName("orbis");
-        oracleDataSource.setServerName("localhost");
-        oracleDataSource.setPortNumber(1521);
-        context.rebind (dataSourceName, oracleDataSource);
     }
 
 }
