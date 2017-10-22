abfaad47306e45d75012d06de160233f456d24a9
lerunchicm@gmail.com <aiY9johxna>
2017-10-23 00:20:27 +0300

    lab4 something shit part 2

--- lab4/src/main/java/me/savushkin/lab4/Lab.java
+++ lab4/src/main/java/me/savushkin/lab4/Lab.java
@@ -22,65 +22,6 @@ public class Lab {
 
     public static void main(String[] args) {
 
-        Connection conn = null;
-        //get properties
-        Properties prop = getPropertiesFromFile();
-        String driver = prop.getProperty("driver1");
-        String jdbcUrl = prop.getProperty("jdbcUrl1");
-        System.setProperty("driver", driver);
-        System.setProperty("url", jdbcUrl);
-        //setProperty();
-        try {
-            //Driver registration
-            System.out.println("Driver: " + System.getProperty("driver"));
-            Class.forName(System.getProperty("driver")).newInstance();
-
-
-            //Create connection
-            System.out.println("Connection to database...");
-            System.out.println("Connection to " + getProperty("url"));
-            conn = DriverManager.getConnection(getProperty("url"));
-            //Executing the query
-            Statement statement = conn.createStatement();
-            String sql = "SELECT COUNT(*) FROM all_tables";
-            statement.execute(sql);
-            statement.close();
-            conn.close();
-        }catch (ClassNotFoundException e){
-            System.out.println("Driver not found");
-            e.printStackTrace();
-    }catch (Exception e){
-            e.printStackTrace();
-        }
-
-    }
-
-    //get property
-    private static Properties getPropertiesFromFile() {
-        Properties prop = new Properties();
-        InputStream input = null;
-        try {
-
-            input = Lab.class.getResourceAsStream("/database.properties");
-            // load a properties file
-            prop.load(input);
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        } finally {
-            if (input != null) {
-                try {
-                    input.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return prop;
-    }
-
-    //set property
-    private static void setProperty() {
 
     }
 }
--- lab4/src/main/java/ru/makhnovets/lab4/Lab.java
+++ lab4/src/main/java/ru/makhnovets/lab4/Lab.java
@@ -1,5 +1,10 @@
 package ru.makhnovets.lab4;
 
+import java.awt.geom.RectangularShape;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Lab {
     //33639
     //3. Не указывать импорт.
@@ -15,6 +20,10 @@ public class Lab {
             "jdbc:oracle:thin:%s/%s@localhost:1521:orbis",
             ORACLE_USER,
             ORACLE_PASS);
+    private static String operatorsql = "CREATE TABLE операторы_связи (ид NUMERIC(3) CONSTRAINT ПК_ОС PRIMARY KEY, оператор VARCHAR(20))";
+    private static String telefonsql = "CREATE TABLE номера_телефонов (ид_л NUMERIC(9) CONSTRAINT ВК_Л REFERENCES н_люди(ид), номер_телефона VARCHAR(20), дата_регистрации DATE, ид_ос NUMERIC(3) CONSTRAINT ВК_ОС REFERENCES операторы_связи(ид))";
+
+
 
     public static void main(String[] args) {
         java.sql.Connection connection = null;
@@ -22,12 +31,26 @@ public class Lab {
         try {
             connection = java.sql.DriverManager.getConnection(ORACLE_URL);
             java.sql.Statement statement = connection.createStatement(
-                    java.sql.ResultSet.TYPE_FORWARD_ONLY, 
+                    java.sql.ResultSet.TYPE_FORWARD_ONLY,
                     java.sql.ResultSet.CONCUR_UPDATABLE);
-            statement.execute("SELECT COUNT(*) FROM all_tables");
+            System.out.println("Create tables");
+            createTables(statement);
+            System.out.println("Insert data");
+            insertData(statement);
+            System.out.println("Select data");
+            selectData(statement);
+
+            String sql="SELECT * FROM операторы_связи WHERE ид=1";
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                if (resultSet.isFirst()) {
+                    resultSet.updateString("оператор","new operator");
+                    System.out.println("chance first row");
+                }
+                continue;
+            }
+            selectData(statement);
             statement.close();
-            
-            
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -39,5 +62,45 @@ public class Lab {
                 e.printStackTrace();
             }
         }
+    }
+    private static void selectData(Statement statement) throws SQLException {
+        String sql="SELECT * FROM операторы_связи";
+        Boolean isRetrieved = statement.execute(sql);
+        System.out.println("Is data retrieved: " + isRetrieved);
+        System.out.println("Displaying retrieved data:");
+        ResultSet resultSet = statement.executeQuery(sql);
+        while (resultSet.next()) {
+            int id = resultSet.getInt("ид");
+            String name = resultSet.getString("оператор");
+
+            System.out.println("ID: " + id);
+            System.out.println("Operator: " + name);
+            //System.out.println("Specialty: " + specialty);
+            //System.out.println("Salary: " + salary);
+            System.out.println("===================");
+        }
+    }
+    private static void createTables(Statement statement) throws SQLException {
+        String operatorsql = "CREATE TABLE операторы_связи (ид NUMERIC(3) CONSTRAINT ПК_ОС PRIMARY KEY, оператор VARCHAR(20))";
+        //String telefonsql = "CREATE TABLE номера_телефонов (ид_л NUMERIC(9) CONSTRAINT ВК_Л REFERENCES н_люди(ид), номер_телефона VARCHAR(20), дата_регистрации DATE, ид_ос NUMERIC(3) CONSTRAINT ВК_ОС REFERENCES операторы_связи(ид))";
+        try {
+            statement.execute("DROP TABLE операторы_связи CASCADE CONSTRAINTS");
+        } catch (SQLException se) {
+            //Игнорировать ошибку удаления таблицы
+            if(se.getErrorCode()==942) {
+                String msg = se.getMessage();
+                System.out.println("Error drop table is fold: "+msg);
+            }
+        }
+        //Создание таблицы операторы_связи
+        if(statement.execute(operatorsql))
+            System.out.println("Table operators create...");
+    }
+    private static void insertData(Statement statement) throws SQLException {
+        //Загрузка данных в таблицу операторы_связи
+        statement.execute("INSERT INTO операторы_связи VALUES(1,'Megafon')");
+        statement.execute("INSERT INTO операторы_связи VALUES(2,'MTC')");
+        statement.execute("INSERT INTO операторы_связи VALUES(3,'Beeline')");
+        statement.execute("INSERT INTO операторы_связи VALUES(4,'SkyLink')");
     }
 }
