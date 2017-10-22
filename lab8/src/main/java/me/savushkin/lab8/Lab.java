1f6155d8f075ea9e504a69a6c7ce15e0497edd0e
Ivan Savushkin <ivan@savushkin.me>
2017-10-23 00:47:06 +0300

    lab8

--- lab8/src/main/java/me/savushkin/lab8/Lab.java
+++ lab8/src/main/java/me/savushkin/lab8/Lab.java
@@ -1,8 +1,6 @@
 package me.savushkin.lab8;
 
-import me.savushkin.common.LabHelper;
 import oracle.jdbc.rowset.OracleCachedRowSet;
-import oracle.jdbc.rowset.OracleJDBCRowSet;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -20,12 +18,11 @@ public class Lab {
     // 3. Создать объект типа CachedRowSet и заполнить его результатами запроса, находящимися в объекте типа ResultSet, методом populate().
     // 4. Сохранить объект типа CachedRowSet в файле, имя которого должно совпадать с идентификатором студента, выполняющего задание, и находиться в корне его домашнего каталога.
 
-    // 23. Определите вашу среднюю оценку (естественно, что в расчет должны входить лишь те оценки, которые имеют цифровой эквивалент). Создайте два запроса, в которых средняя оценки определяется с помощью функции среднее значение (функция AVG) и путем деления суммы (функция SUM) оценок на их количество (функция COUNT). В результате необходимо оставить два десятичных знака после запятой.
-    // Для округления используется функция ROUND(expr [,m]), возвращающая expr, округленное до m-го десятичного знака; если m опущено, то оно принимается равным 0, а если m < 0, то округляются цифры левее десятичной точки.
+    // 23. Определите вашу среднюю оценку (естественно, что в расчет должны входить лишь те оценки, которые имеют цифровой эквивалент). Создайте два запроса, в которых средняя оценки определяется с помощью функции среднее значение (функция AVG) и путем деления суммы (функция SUM) оценок на их количество (функция COUNT). В результате необходимо оставить два десятичных знака после запятой. Для округления используется функция ROUND(expr [,m]), возвращающая expr, округленное до m-го десятичного знака; если m опущено, то оно принимается равным 0, а если m < 0, то округляются цифры левее десятичной точки.
     private final static String ORACLE_URL = "jdbc:oracle:thin:@localhost:1521:orbis";
     private final static String ORACLE_DRIVER = "oracle.jdbc.driver.OracleDriver";
-    private final static String ORACLE_USER = "s182190";
-    private final static String ORACLE_PASS = "tny395";
+    private final static String ORACLE_USER = "";
+    private final static String ORACLE_PASS = "";
 
     private final static List<String> QUERY_MARK = Arrays.asList(
             "SELECT Л.ФАМИЛИЯ, Л.ИМЯ, Л.ОТЧЕСТВО, О.СРЕДНЯЯ_ОЦЕНКА FROM Н_ЛЮДИ Л JOIN (SELECT В.ЧЛВК_ИД, ROUND(AVG(CAST(В.ОЦЕНКА AS DECIMAL(1, 0))), 2) СРЕДНЯЯ_ОЦЕНКА FROM Н_ВЕДОМОСТИ В WHERE В.ОЦЕНКА IN ('2','3','4','5') GROUP BY В.ЧЛВК_ИД) О ON Л.ИД = О.ЧЛВК_ИД WHERE Л.ИД = ?",
@@ -38,39 +35,24 @@ public class Lab {
     public static void main(String[] argv) {
         try {
             Class.forName(ORACLE_DRIVER);
-
             try (Connection connection = DriverManager.getConnection(ORACLE_URL, ORACLE_USER, ORACLE_PASS)) {
+                int i = 1;
                 for (String query: QUERY_MARK)
-                    try (OracleCachedRowSet oracleCachedRowSet = new OracleCachedRowSet();) {
-                        oracleCachedRowSet.setCommand(query);
-                        try (ObjectOutputStream objectOutputStream =
-                                     new ObjectOutputStream(
-                                             new FileOutputStream(oracleCachedRowSet.getTableName()))) {
-                            objectOutputStream.writeObject(oracleCachedRowSet);
-                            objectOutputStream.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
+                    try (PreparedStatement statement = connection.prepareStatement(query);) {
+                        statement.setString(1, STUDENT_ID);
+                        try (ResultSet resultSet = statement.executeQuery()) {
+                            OracleCachedRowSet oracleCachedRowSet = new OracleCachedRowSet();
+                            oracleCachedRowSet.populate(resultSet);
+
+                            try (ObjectOutputStream objectOutputStream =
+                                         new ObjectOutputStream(
+                                                 new FileOutputStream(String.format("S182190-%d", i)))) {
+                                objectOutputStream.writeObject(oracleCachedRowSet);
+                                objectOutputStream.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
                         }
-
-
-//                    while(resultSet.next()) {
-//                        int columnsCount = metaData.getColumnCount();
-//                        for (int column = 1; column <= columnsCount; column++) {
-//                            StringBuilder stringBuilder = new StringBuilder();
-//                            stringBuilder.append(metaData.getColumnName(column)).append(": ");
-//                            switch (metaData.getColumnType(column)) {
-//                                case 2:
-//                                    stringBuilder.append(resultSet.getDouble(column));
-//                                    break;
-//                                case 12:
-//                                    stringBuilder.append(resultSet.getString(column));
-//                                    break;
-//                            }
-//                            System.out.println(stringBuilder.toString());
-//                        }
-//                        System.out.println();
-//                    }
-
                     }
             }
         } catch (ClassNotFoundException | SQLException e) {
